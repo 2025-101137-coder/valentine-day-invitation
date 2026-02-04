f4b0a52c8be1693466b6a89c7d011c8a96a6f7be
2025-101137-coder <2025-101137@rtu.edu.ph>
2026-02-05 00:49:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let leftPos = 0
 let bottomPos = 0
 document.getElementById('yessir').addEventListener('click', () => {
-    alert('Congratulations! You have been invited to a Valentine\'s Day dinner. Please RSVP by February 10th. We hope to see you there!')
+    alert('Hi Ishh! You’re warmly invited to spend Valentine’s Day with me. It would mean a lot to share this special day together. Let’s enjoy good moments, simple laughs, and meaningful conversations. Your presence alone would make the day more special. I hope you can join me and make this Valentine’s Day memorable. 💌
+')
 })
 document.getElementById('nooo').addEventListener('mouseover', () => {
     leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
